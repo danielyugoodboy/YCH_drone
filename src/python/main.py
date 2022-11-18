9fe3714bd0accbff004256db1c95106470538a01
YuChiaHao <danielyugoodboy@gmail.com>
2022-11-18 17:12:18 +0800

    2022/11/18 Start keyboard control

--- src/python/main.py
+++ src/python/main.py
@@ -8,14 +8,17 @@ from geometry_msgs.msg import PoseStamped
 from mavros_msgs.msg import State
 from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
 
+CONMAND_FPS = 25
+
 '''
-參考文件：https://github.com/danielyugoodboy/NCRL-AIDrone-Platform/tree/master/src
-飛行模式：https://docs.px4.io/main/zh/getting_started/flight_modes.html
+Ref：https://github.com/danielyugoodboy/NCRL-AIDrone-Platform/tree/master/src
+Flying Mode：https://docs.px4.io/main/zh/getting_started/flight_modes.html
 MAVROS Basics: http://edu.gaitech.hk/gapter/mavros-basics.html
 MAVROS_Tutorial: https://masoudir.github.io/mavros_tutorial/
 
-設計不同的飛行模式：
-
+Design different flying mode：
+1.RPYT
+2.Position
 
 '''
 
@@ -33,7 +36,7 @@ def main(args):
     print("State: Start Drone Command")
     rospy.init_node("offb_node_py")
 
-    # ******************** PX4 SETTING ******************** #
+    # ************************* PX4 SETTING ************************* #
 
     # Set subscriber & publisher
     state_sub = rospy.Subscriber("mavros/state", State, callback = state_cb)
@@ -41,59 +44,60 @@ def main(args):
 
     # Set client
     print("State: Wait for Arming service")
-    rospy.wait_for_service("/mavros/cmd/arming")  # 等待 service 可用
+    rospy.wait_for_service("/mavros/cmd/arming")  # wait service
     arming_client = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)
 
     print("State: Wait for Set_mode service")
-    rospy.wait_for_service("/mavros/set_mode")  # 等待 service 可用
+    rospy.wait_for_service("/mavros/set_mode")  # wait service
     set_mode_client = rospy.ServiceProxy("mavros/set_mode", SetMode)
 
     # Setpoint publishing MUST be faster than 2Hz
-    rate = rospy.Rate(20)
+    rate = rospy.Rate(CONMAND_FPS)
 
     # Wait for Flight Controller connection
     print("State: Wait for Connection")
     while(not rospy.is_shutdown() and not current_state.connected):
         rate.sleep()
-    print("State: Connected !")
-
-    # Define inital position
-    pose = PoseStamped()
-    pose.pose.position.x = 2
-    pose.pose.position.y = 0
-    pose.pose.position.z = 2
-
-    # Send a few setpoints before starting
-    for i in range(50):   
-        if(rospy.is_shutdown()):
-            break
-        else:
-            print("Wait for setpoint : " + "{}".format((i+1)*2) + " % ", end='\r')
-            local_pos_pub.publish(pose)
-            rate.sleep()
 
     print("State: Set SetModeRequest")
     offb_set_mode = SetModeRequest()
     offb_set_mode.custom_mode = 'OFFBOARD'
 
-    land_set_mode = SetModeRequest()
-    land_set_mode.custom_mode = 'LAND'
-
     print("State: Set CommandBoolRequest")
     arm_cmd = CommandBoolRequest()
     arm_cmd.value = True
 
-    last_req = rospy.Time.now()
+    # ************************* MAIN FUCTION ************************ #
 
-    print("State: Start")
     try:
+        # Define inital position
+        pose = PoseStamped()
+        pose.pose.position.x = 0
+        pose.pose.position.y = 0
+        pose.pose.position.z = 1
+
+        # Publish inital point before start
+        for i in range(100):   
+            if(rospy.is_shutdown()):
+                break
+            else:
+                print("Wait for setpoint : " + "{}".format(i+1) + " % ", end='\r')
+                local_pos_pub.publish(pose)
+                rate.sleep()
+        
+        
+        print("State: Start")
+        last_req = rospy.Time.now()
+        init_time = time.time()
         while(not rospy.is_shutdown()):
+            # Confirm current_state.mode == "OFFBOARD"，give 5 sec to wait
             if(current_state.mode != "OFFBOARD" and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
                 if(set_mode_client.call(offb_set_mode).mode_sent == True):
                     rospy.loginfo("OFFBOARD enabled")
 
                 last_req = rospy.Time.now()
 
+            # Confirm current_state.armed == True，give 5 sec to wait
             else:
                 if(not current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
                     if(arming_client.call(arm_cmd).success == True):
@@ -101,15 +105,20 @@ def main(args):
 
                     last_req = rospy.Time.now()
 
+            # Publish pose
             local_pos_pub.publish(pose)
+            pose.pose.position.x += 0.005
+
+            current_time = time.time()
+            print("Control Conmand FPS : " + "{:.1f}".format(1/(current_time-init_time)), end='\r')
+            init_time = current_time
 
             rate.sleep()
 
     except KeyboardInterrupt:
-        print('KeyboardInterrupt!')
+        print('Keyboard Interrupt!')
 
     finally:
-        set_mode_client.call(land_set_mode).mode_sent
         rospy.on_shutdown(myhook)
 
 if __name__ == "__main__":
