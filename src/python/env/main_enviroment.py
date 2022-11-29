2022/11/29 fix Quaternion to Eularian

src/python/env/main_enviroment.py
@@ -228,6 +228,7 @@ class Drone_Enviroment():
 
     def _np2PoseStamped(self, np_action):
         '''
+        Eularian to Quaternion
         這邊的姿態使用的是四元數（Quaternion）
         q = cos(theta/2)+sin(theta/2)n
         q = cos(theta/2)+sin(theta/2)i+sin(theta/2)j+sin(theta/2)k
@@ -246,7 +247,10 @@ class Drone_Enviroment():
 
     def _PoseStamped2np(self, pose):
         '''
+        Quaternion to Eularian
+        https://www.cnblogs.com/21207-ihome/p/6894128.html
         這邊的姿態使用的是四元數（Quaternion）
+
         q = cos(theta/2)+sin(theta/2)n
         q = cos(theta/2)+sin(theta/2)i+sin(theta/2)j+sin(theta/2)k
         q = w + xi + yj + zk
@@ -254,24 +258,25 @@ class Drone_Enviroment():
         第一二象限： z:- w:- or z:+ w:+
         第三四象限： z:+ w:- or z:- w:+
         '''
-        X = pose.pose.position.x
-        Y = pose.pose.position.y
-        Z = pose.pose.position.z
+        pos_X = pose.pose.position.x
+        pos_Y = pose.pose.position.y
+        pos_Z = pose.pose.position.z
         
-        if pose.pose.orientation.w >1:
-            pose.pose.orientation.w = 1
-        elif pose.pose.orientation.w < -1:
-            pose.pose.orientation.w = -1
+        q_w = pose.pose.orientation.w
+        q_x = pose.pose.orientation.x
+        q_y = pose.pose.orientation.y
+        q_z = pose.pose.orientation.z
+
+        # roll
+        #roll = math.atan2(2*(q_w*q_x + q_y*q_z), 1-2*(q_x**2 + q_y**2))
         
-        # 第一二象限
-        if (pose.pose.orientation.z*pose.pose.orientation.w)>0:
-            yaw = math.acos(abs(pose.pose.orientation.w))*2
+        # pitch
+        #pitch  = math.asin(np.clip(2*(q_w*q_y - q_z*q_z), -1.0, 1.0))
 
-        # 第三四象限
-        else:
-            yaw = -math.acos(abs(pose.pose.orientation.w))*2
+        # yaw
+        yaw = math.atan2(2*(q_w*q_z + q_x*q_y), 1-2*(q_y**2 + q_z**2))
 
-        return np.array([[X,Y,Z],[0,0,yaw]])
+        return np.array([[pos_X,pos_Y,pos_Z],[0, 0, yaw]])
 
     def _myhook(self):
         print("[State] : ROS shutdown, Drone Back to home !")

src/python/pj01_keyboard_control.py
@@ -12,7 +12,6 @@ Done = False
 observation = None
 action = None
 press_time = time.time()
-command = False
 
 class TK_KeyBoardThread(threading.Thread):
     def __init__(self):
@@ -25,20 +24,20 @@ class TK_KeyBoardThread(threading.Thread):
     
     def xFunc_press(self, event):
         # forward / go_back / left / right
-        global action, press_time, command
+        global action, press_time
         if action is not None and self.start_control:
 
             if event.char == 'w':
-                if action[0][0] < 10: 
+                if action[0][0] < 5: 
                     action[0][0]+=0.1
             elif event.char == 's':
-                if action[0][0] > -10: 
+                if action[0][0] > -5: 
                     action[0][0]-=0.1
             elif event.char == 'a':
-                if action[0][1] < 10: 
+                if action[0][1] < 5: 
                     action[0][1]+=0.1
             elif event.char == 'd':
-                if action[0][1] > -10: 
+                if action[0][1] > -5: 
                     action[0][1]-=0.1
             
             # up / down / counterClockwise /Clockwise
@@ -49,14 +48,13 @@ class TK_KeyBoardThread(threading.Thread):
                 if action[0][2] > -5: 
                     action[0][2]-=0.1
             elif event.char == 'j':
-                if action[1][2] < 1: 
-                    action[1][2]+=0.05
+                if action[1][2] < 0.5: 
+                    action[1][2]+=0.1
             elif event.char == 'l':
-                if action[1][2] > -1: 
-                    action[1][2]-=0.05
+                if action[1][2] > -0.5: 
+                    action[1][2]-=0.1
             
             press_time = time.time()
-            command = True
 
     def run(self):
         global Done
@@ -76,7 +74,7 @@ class TK_KeyBoardThread(threading.Thread):
 
 def main():
     global Done
-    global action, press_time, command
+    global action, press_time
 
     env = ENV()
     observation = env.reset()
@@ -90,19 +88,18 @@ def main():
     # Control Loop
     while True:
         # make drone stable
-        if (time.time()-press_time)>0.1:
-            action = action/1.5
-            
-        elif (time.time()-press_time)>1 and command:
+        if (time.time()-press_time)>0.1 :
+            action = action/4
+            action[1][2] = 0
+
+        elif (time.time()-press_time)>0.5 :
             action = np.array([[0,0,0],[0,0,0]])
-            command = False
         
         cur_img = observation.img
         cv2.imshow("Image window", cur_img)
         cv2.waitKey(3)
 
         #print("Action XYZyaw : {:.2f}, {:.2f}, {:.2f}, {:.2f}".format(action[0][0], action[0][1], action[0][2], action[1][2]), end='\r')
-        print(env._PoseStamped2np(env.current_pos)[1][2])
         next_observation, reward, done, info = env.velocity_step(action)
         observation = next_observation
         if Done: