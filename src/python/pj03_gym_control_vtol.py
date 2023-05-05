b0e8c91b83ce4251c41f7392cf8aea1eabc56e80
YuChiaHao <danielyugoodboy@gmail.com>
2023-05-05 17:06:51 +0800

    新增vtol地面站

---
+++ src/python/pj03_gym_control_vtol.py
@@ -0,0 +1,39 @@
+import numpy as np
+import argparse
+from env.main_enviroment_vtol import Drone_Enviroment as ENV
+
+# Design Agent Start
+class myAgent():
+    def __init__(self):
+        pass
+
+    def select_action(self,obs):
+        x_dir, y_dir, z_dir = 20, 0, 0
+        pitch, row, yaw = 0, 0, 0
+
+        action = np.array([[x_dir, y_dir, z_dir], [pitch, row, yaw]])
+        return action
+
+# Gym like
+def main(args):
+    env = ENV()
+    agent = myAgent()
+    
+    for ep in range(args.ep):
+        observation = env.reset()
+        print("Start Episode : {}".format(ep+1))
+        while True:
+            action = agent.select_action(observation)
+            next_observation, reward, done, info = env.velocity_step(action)
+            observation = next_observation
+            if done:
+                break
+    
+    env.reset()
+    env.shotdown()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ep", default=1, type=int, help="Episode Number")
+    args = parser.parse_args()
+    main(args)
